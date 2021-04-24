a8026b774b20a6a492950b13934b0ed7dd616cf7
Yesid Cadavid Carvalho <Ycadavid@intergrupo.com>
2021-04-24 17:10:40 -0500

    Ajustes en mensajes swal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { fromEvent, merge, Observable, Observer } from 'rxjs';
+import { IndexedDb } from './config/indexedDb/IndexedDb';
+import { map } from 'rxjs/operators';
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProgramaTuBreak';
+  title = 'Programa Tu Break';
+
+  constructor(private IndexedDb: IndexedDb) {
+
+    this.createOnline$().subscribe(isConnected => {
+      this.IndexedDb.createDB();
+      if (isConnected) {
+        swal.fire('En Linea', 'Volviste a tener conexión!', 'success');
+      } else {
+        swal.fire('Offline', 'puedes seguir con lo tuyo! en cuanto recuperemos la conexion sincronizaremos ;)', 'warning');
+      }
+      console.log(isConnected ? 'En Linea' : 'Offline');
+    });
+  }
+
+  createOnline$() {
+    return merge<boolean>(
+      fromEvent(window, 'offline').pipe(map(() => false)),
+      fromEvent(window, 'online').pipe(map(() => true)),
+      new Observable((sub: Observer<boolean>) => {
+        sub.next(navigator.onLine);
+        sub.complete();
+      }));
+  }
 }
---
+++ src/app/config/indexedDb/IndexedDb.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { deleteDB, openDB } from 'idb';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IndexedDb {
+  dbName = 'ProgramaTuBreak';
+
+  async createDB() {
+    await openDB(this.dbName, 1, {
+      upgrade(db) {
+        db.createObjectStore('formulario', { keyPath: 'guid' });
+        db.createObjectStore('productos', { keyPath: 'guid' });
+        db.createObjectStore('maestros', { keyPath: 'guid' });
+      }
+    });
+  }
+  
+  async deleteDB() {
+    await deleteDB(this.dbName);
+  }
+
+  async updateDB() {
+    const numberTablesDB = 3;
+    const dbProgramaTuBreak = await openDB(this.dbName, 1);
+    if (dbProgramaTuBreak.objectStoreNames.length < numberTablesDB) {
+      this.deleteDB();
+      this.createDB();
+    }
+  }
+
+}
